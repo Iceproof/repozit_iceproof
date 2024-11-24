3e14ea2a18dadeb912262f6e6ba27161376f023b
Iceproof <andrey.vesselov@gmail.com>
2024-11-24 09:28:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Тестовое задание Веселов Андрей</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+<div class="zero"> </div>
+   <?php require 'nav.php';?>
+   <div class="zero"> </div>
+
+	<div class="goods">
+
+				<div class="row">
+        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img1.jpg" alt="Одежда"> </a>
+            <div class="card-body">
+              <div class="card-title">Одежда</div>
+            </div>
+          </div>
+        </div>
+		
+		        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img2.webp" alt="Продукты"> </a>
+            <div class="card-body">
+              <div class="card-title">Продукты</div>
+            </div>
+          </div>
+        </div>
+		
+		        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img3.webp" alt="Красота и здоровье"> </a>
+            <div class="card-body">
+              <div class="card-title">Красота и здоровье</div>
+            </div>
+          </div>
+        </div>
+				</div>
+
+
+				<div class="row">
+        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img4.webp" alt="Автомобиль"> </a>
+            <div class="card-body">
+              <div class="card-title">Автомобиль</div>
+            </div>
+          </div>
+        </div>
+		
+		        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img5.webp" alt="Зоотовары"> </a>
+            <div class="card-body">
+              <div class="card-title">Зоотовары</div>
+            </div>
+          </div>
+        </div>
+		
+		        <div class="col">
+          <div class="card">
+			<a href="temp.php"> <img src="img/img6.jpg" alt="Детские товары"> </a>
+            <div class="card-body">
+              <div class="card-title">Детские товары</div>
+            </div>
+          </div>
+        </div>
+				</div>
+
+			
+	</div>		
+			
+			
+						
+</body>
+
+</html>
